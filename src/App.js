bb31af9367afdb562f2dd169dab0af9716e183ea
Erhan Namal <namallerhan@gmail.com>
2020-09-21 16:25:02 +0300

    fix eslint error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import styled from "styled-components";
+
 import Theme from "./components/Theme";
 import Header from "./components/Header";
-import styled from "styled-components";
 
 function App() {
   return (
